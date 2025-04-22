3bca330d51afcd1cb8d3eaf1e00a606297737ddb
Andrew Hawes <117602754+Andrew1Hawes@users.noreply.github.com>
2025-04-22 04:30:54 +0100

    Add my copyright(left)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 /*
  * Copyright (C) 2017 FIX94
+ * Copyright (C) 2025 Lynx64
  *
  * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
+ * of the MIT license. See the LICENSE file for details.
  */
 #include "main.h"
 
